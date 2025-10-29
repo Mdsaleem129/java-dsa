d8bce167458fd14cd779aa3548857e0121009516
Mohdsaleem129 <mohdsaleem952@gmail.com>
2025-10-29 19:18:01 +0530

    newjava file

---
+++ git.java
@@ -0,0 +1,8 @@
+public class git {
+    public static void main(String[] args) {
+        System.out.println("loop");
+        for(int i=0;i<=10;i++){
+            System.out.println("pushing  to git hub");
+        }
+    }
+}
